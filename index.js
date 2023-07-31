f88a0b2da960533ff5c5ac6e7ff5f99e52a7f1a3
Greydi Lora <glora2019.fau.edu>
2023-07-31 15:25:30 -0400

    Start on the JS

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const projectsEl = document.querySelector(".main")
+let projectsHTML = "";
+
+projects.forEach((project) => {
+  projectsHTML += `
+  <div class="project-container">
+  <div class="image"><img src="${project.image}" /></div>
+  <div class="title">Project Name</div>
+  <div class="description">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+    iste, minima dolorem, debitis sunt accusamus impedit quia asperiores
+    ipsum possimus cupiditate quidem assumenda autem libero eaque.
+    Voluptatibus aliquam impedit molestiae?
+  </div>
+</div>`
+})
+
+document.querySelector(".main").innerHTML = projectsHTML;
 

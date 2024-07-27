7565388e1b55caf33fbcf13fc158b555272e4199
Greydi <greydi.lora@yahoo.com>
2024-07-26 21:36:47 -0400

    Finished recipe book

--- recipe/recipe.js
+++ recipe/recipe.js
@@ -1,13 +1,48 @@
 const API_KEY = "0152c441a4c9415da644b3c9b15a9b7c";
 const recipeListEl = document.getElementById("recipe-list")
 
+
+
+
 function displayRecipes(recipes) {
+
 recipeListEl.innerHTML = ""
 recipes.forEach((recipe) => {
-  const recipeItemEl = document.createElement("li")
-  recipeItemEl
+  const recipeItemEl = document.createElement("li");
+  recipeItemEl.classList.add("recipe-item");
+
+  recipeImageEl = document.createElement("img");
+  recipeImageEl.src = recipe.image;
+  recipeImageEl.alt = "recipe image";
+
+
+  recipeTitleEl = document.createElement("h2");
+  recipeTitleEl.innerText = recipe.title;
+
+  recipeIngredientEl = document.createElement("p")
+  recipeIngredientEl.innerHTML = `
+  <strong>Ingredients:</strong> ${recipe.extendedIngredients.map((ingredient)=>ingredient.original).join(",")}
+  `
+
+  recipeLinkEl = document.createElement("a")
+  recipeLinkEl.href = recipe.sourceUrl;
+  recipeLinkEl.innerText = "View Recipe";
+ 
+
+
+  recipeItemEl.appendChild(recipeImageEl);
+  recipeItemEl.appendChild(recipeTitleEl);
+  recipeItemEl.appendChild(recipeIngredientEl);
+  recipeItemEl.appendChild(recipeLinkEl);
+  recipeListEl.appendChild(recipeItemEl);
+  
 })
 }
+
+
+
+
+
 
 async function getRecipes() {
   const response = await fetch(
@@ -17,6 +52,11 @@ async function getRecipes() {
   const data = await response.json();
   return data.recipes;
 }
+
+
+
+
+
 
 
 async function init() {
